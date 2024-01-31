f0323f76d0de74c513b2cd95d79ac4dd6cf5d76f
Bryan Besnyi <besnyib@smccd.edu>
2024-01-31 10:23:50 -0800

    🐛 FIX: commented auth test page

--- src/app/admin/client/page.tsx
+++ src/app/admin/client/page.tsx
@@ -1,44 +1,44 @@
-'use client'
+// 'use client'
 
-import { useSession } from 'next-auth/react'
+// import { useSession } from 'next-auth/react'
 
-async function getData() {
-  const res = await fetch('http://localhost:3000/people')
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
+// async function getData() {
+//   const res = await fetch('http://localhost:3000/people')
+//   // The return value is *not* serialized
+//   // You can return Date, Map, Set, etc.
 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
+//   if (!res.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error('Failed to fetch data')
+//   }
 
-  return res.json()
-}
+//   return res.json()
+// }
 
-export default async function ClientPage() {
-  const { data: session } = useSession({
-    required: true,
-  })
+// export default async function ClientPage() {
+//   const { data: session } = useSession({
+//     required: true,
+//   })
 
-  const personData = await getData()
+//   const personData = await getData()
 
-  return (
-    <div className="mt-5 prose">
-      <h1>Protected Page</h1>
-      <p>
-        This page is protected on the client side. You can only see this page if
-        you are signed in.
-      </p>
-      <ul>
-        {{personData.map((person) => (
-          <li key={person.name}>{person.name}</li>
-        ))}}
-      </ul>
-      <p>
-        You are signed in as <strong>{session?.user?.email}</strong>.
-      </p>
-      <p>Session data:</p>
-      <pre>{JSON.stringify(session, null, 2)}</pre>
-    </div>
-  )
-}
+//   return (
+//     <div className="mt-5 prose">
+//       <h1>Protected Page</h1>
+//       <p>
+//         This page is protected on the client side. You can only see this page if
+//         you are signed in.
+//       </p>
+//       {/* <ul>
+//         {{personData.map((person) => (
+//           <li key={person.name}>{person.name}</li>
+//         ))}}
+//       </ul> */}
+//       <p>
+//         You are signed in as <strong>{session?.user?.email}</strong>.
+//       </p>
+//       <p>Session data:</p>
+//       <pre>{JSON.stringify(session, null, 2)}</pre>
+//     </div>
+//   )
+// }
