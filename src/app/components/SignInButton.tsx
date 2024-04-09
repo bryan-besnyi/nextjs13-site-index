97995249314ce0c2e878bf1dbd120bc41bf679a2
Bryan Besnyi <besnyib@smccd.edu>
2024-04-09 15:02:54 -0700

    🐛 FIX: Update callbackUrl in SignInButton.tsx

--- src/app/components/SignInButton.tsx
+++ src/app/components/SignInButton.tsx
@@ -11,7 +11,12 @@ const OneLoginSignInButton = () => {
       <button
         className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
         onClick={() =>
-          signIn('onelogin', { callbackUrl: 'http://localhost:3000/admin' })
+          signIn('onelogin', {
+            callbackUrl:
+              process.env.NODE_ENV === 'production'
+                ? 'https://site-index.smccd.edu/admin'
+                : 'http://localhost:3000/admin'
+          })
         }
       >
         Continue with MySMCCD
